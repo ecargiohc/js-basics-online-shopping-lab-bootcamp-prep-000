68d73ff529017f6ba8fb15e93cb77b627c394faf
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   for (let i = 0; i < cart.length; i++); {
     if (cardNumber === 0); {
-      return "Sorry, we don't have a credit card on file for you.";
+      return `Your total cost is ${total}, which will be charged to the card ${placeOrder}.`;
     }
   }
     if (cart.length === 0); {
