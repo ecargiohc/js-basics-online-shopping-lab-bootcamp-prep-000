c3209c47bd6a497f01b0d182798a7847625ca768
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,10 +62,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   for (i = 0; i < cart.length; i++); {
     if (cardNumber === 0);{
-    return "Sorry, we don't have a credit card on file for you."; 
+      return "Sorry, we don't have a credit card on file for you."; 
   }
     else {
-    return "Your total cost is $71, which will be charged to the card 83296759.";
+      return "Your total cost is $71, which will be charged to the card 83296759.";
     }
   }
 }
