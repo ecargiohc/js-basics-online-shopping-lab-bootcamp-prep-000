ee5cd4a0cf711b863c9a6c5d0ac7a15c7baf4208
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:34:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
       return "Sorry, we don't have a credit card on file for you.";
     }
   }
-    if (); {
+    if (cardNumber ); {
       cart = [];
       return `Your total cost is ${total}, which will be charged to the card ${placeOrder}.`;
     }
