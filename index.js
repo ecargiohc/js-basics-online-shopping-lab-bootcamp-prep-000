d2da7ef279d8567244e896b573d2ef5e12d67aa4
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:09:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,12 +51,11 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
-  if (item = cart[i].itemName) { 
-  cart.splice(item);
+  if (item === cart[i].itemName) { 
+  cart.splice(i, 1);
   return cart
   }
     return "That item is not in your cart.";
-  }
   }
 }
 
