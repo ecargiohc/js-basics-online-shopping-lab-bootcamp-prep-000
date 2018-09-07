59fed1f7cc6b04170f5fd2a5712c87d66e9bff7e
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function placeOrder(cardNumber) {
     }
   }
     if (cart.length === 0); {
-      cart = []
+      cart = [];
       return `Your total cost is ${total}, which will be charged to the card ${placeOrder}.`;
-  
+    }
 }
