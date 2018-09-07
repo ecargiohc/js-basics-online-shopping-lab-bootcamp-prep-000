7a10eddbd105c4d5fb3fa5fa24b007e3c8db8b5c
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:12:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ function removeFromCart(item) {
   }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0);
+  if (cardNumber === 0);{
     return "Sorry, we don't have a credit card on file for you."
+  }
+  else if ()
+  }
 }
