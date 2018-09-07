f99aa136db6ac740c8a18aad9a1604721d9b2169
gc6123@bard.edu <gc6123@bard.edu>
2018-09-07 22:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,6 +64,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return "Your total cost is $71, which will be charged to the card 83296759."
+    return "Your total cost is $71, which will be charged to the card 83296759.";
   }
 }
